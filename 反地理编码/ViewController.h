7b1c3e87e092bad530cfda51fa1886ef03fc2652
1025550 <1796547493@qq.com>
2016-05-30 17:26:12 +0800

    sss
    第一次

--- 反地理编码/ViewController.h
+++ 反地理编码/ViewController.h
@@ -9,6 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *longitude;
+
+@property (weak, nonatomic) IBOutlet UITextField *latitude;
+
+@property (weak, nonatomic) IBOutlet UILabel *reverseDetailAddressLabel;
+
+- (IBAction)reveserGeocode:(id)sender;
 
 
 @end
--- 反地理编码/ViewController.m
+++ 反地理编码/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
-
+#import <CoreLocation/CoreLocation.h>
 @interface ViewController ()
 
+//地址编码的对象
+@property(nonatomic,strong)CLGeocoder *geocoder;
 @end
 
 @implementation ViewController
@@ -19,9 +21,37 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)reveserGeocode:(id)sender {
+    
+    //获取用户出入的经纬度
+    NSString  *longtitude=self.longitude.text;
+    NSString *latitude=self.latitude.text;
+    
+    if (longtitude.length==0||longtitude==nil||latitude.length==0||latitude==nil) {
+        NSLog(@"请输入经纬度");
+        return;
+    }
+    //2.根据用户出入的经纬度创建CLlocation对象
+    CLLocation *locayion=[[CLLocation alloc]initWithLatitude:[latitude doubleValue] longitude:[longtitude doubleValue]];
+    [self.geocoder reverseGeocodeLocation:locayion completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        for (CLPlacemark *place in placemarks) {
+//            NSLog(@"城市:%@ 名字:%@ 经度:%f 纬度%f",place.name,place.addressDictionary,place.location.coordinate.longitude,place.location.coordinate.latitude);
+            
+            self.reverseDetailAddressLabel.text=place.locality;
+            NSLog(@"1111111111");
+            
+        }
+        
+    }];
 }
 
+//懒加载
+-(CLGeocoder *)geocoder{
+    if (!_geocoder) {
+        _geocoder=[[CLGeocoder alloc]init];
+    }
+
+    return _geocoder;
+}
 @end
